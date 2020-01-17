75ebd8586aefee1dfb6f26bbe91cff869eb8c7fb
Rob Cruz <alekseicruz@gmail.com>
2020-01-16 17:51:11 -0800

    initial checkin

---
+++ assets/scripts/app.js
@@ -0,0 +1,43 @@
+
+//const passwordElement = document.querySelector('#password')
+
+console.log(getPassword())
+console.log(getPassword())
+console.log(getPassword())
+
+function getRandCharSet(){
+  switch(Math.floor(Math.random() * (4)) + 1){
+    case 1:
+      return "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    case 2:
+      return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    case 3:
+      return "abcdefghijklmnopqrstuvwxyz"
+    case 4:
+      return "0123456789"
+  }
+}
+
+function getRandNum(min=8, max=128){
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function getRandChar(str){
+  return str.charAt(getRandNum(0, str.length))
+}
+
+function getPassword(){
+  let passwordLength = getRandNum()
+  let password = ""
+  while (password.length < passwordLength){
+    password += getRandChar(getRandCharSet())
+  }
+
+  return password
+}
+
+
+
+
